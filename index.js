821ce9bb03a71177afeeacce3de0265fd7c6aa26
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-10-05 10:39:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+  return fetch('http://localhost:3000/users', {
+      method : 'POST',
+      headers : {
+
+          "content-type" : "application/json",
+          Accept : "application/json"
+      },
+      body : JSON.stringify({
+          name : name,
+          email: email
+      }),
+  })
+  .then(res => res.json())
+  .then((data) => {
+      const dataID = document.createElement('p').textContent = `ID: ${data.id}`
+      document.body.append(dataID)
+  })
+  .catch((error) => {
+      const errorMesg = document.createElement('p').textContent = `Error: ${error.message}`
+      document.body.append(errorMesg) 
+  })
+}
